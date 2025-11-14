204a94b41453b1b06e0cf262a85c7dd5a5e6a896
binaya346 <rijalbinaya@gamil.com>
2025-11-14 08:20:13 +0545

    adds navbar component

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,16 @@
+import Navbar from './navbar';
 
 const Header = (props) => {
     const { top, main, company } = props;
     const number = 2;
-    
+
 
     return (
         <header>
             <div>{top} &nbsp; &nbsp; &nbsp; {company}</div>
             <div>{main}</div>
             <div>{number}</div>
+            <Navbar />
         </header>
     )
 }
