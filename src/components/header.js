073c761556c691edd2b3faef7533e7a3013fc0ba
binaya346 <rijalbinaya@gamil.com>
2025-12-30 08:23:52 +0545

    adds login/logout in header

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,27 @@ import Navbar from './navbar';
 import styles from './header.module.css';
 import Input from './bootstrap/input';
 import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = (props) => {
     const { top, company } = props;
+    const [token, setToken] = useState(localStorage.getItem("accessToken"))
+
+    const navigate = useNavigate();
+
+    const handleRedirectToSignin = () => {
+        navigate("/login")
+    }
+
+    const handleRedirectToRegister = () => {
+        navigate("/register")
+    }
+
+    const handleLogout = () => {
+        localStorage.removeItem("accessToken")
+        setToken("")
+    }
 
     return (
         <header>
@@ -14,14 +32,24 @@ const Header = (props) => {
 
             <Navbar />
 
-            <div className={styles['header-right']}>
-                <div className={styles['btn-wrapper']}>
-                    <Button>Sign In</Button>
-                </div>
-                <div className={styles['btn-wrapper']}>
-                    <Button>Register</Button>
-                </div>
-            </div>
+            {token && token !== "undefined" && token !== "" ?
+                (
+                    <div className={styles['header-right']}>
+                        <div className={styles['btn-wrapper']}>
+                            <Button onClick={handleLogout}>logout</Button>
+                        </div>
+                    </div>
+                ) :
+                (
+                    <div className={styles['header-right']}>
+                        <div className={styles['btn-wrapper']}>
+                            <Button onClick={handleRedirectToSignin}>Sign In</Button>
+                        </div>
+                        <div className={styles['btn-wrapper']}>
+                            <Button variant='light' onClick={handleRedirectToRegister}>Register</Button>
+                        </div>
+                    </div>
+                )}
         </header>
     )
 }
